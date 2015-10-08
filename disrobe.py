d68c67851f488f112a4a4107c7556b1bebb16eb6
Josh Keys <jgkeys@gmail.com>
2015-10-08 09:26:51 -0500

    Formalize return data

--- disrobe.py
+++ disrobe.py
@@ -12,7 +12,12 @@ class GetIP(Resource):
         payload = {}
         remote_addy = request.remote_addr
         x_forward_info = request.headers.get('X-Forwarded-For')
-        payload['data'] = {'X-Forwarded-For': x_forward_info, 'remote_ip': remote_addy}
+        payload['meta'] = {'X-Forwarded-For': x_forward_info, 'remote_ip': remote_addy}
+        if x_forward_info:
+            client_addy = x_forward_info.split(',')[0]
+        else:
+            client_addy = remote_addy
+        payload['data'] = client_addy
         return payload
 
 api.add_resource(GetIP, '/', endpoint='getip')
